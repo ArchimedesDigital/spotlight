4c70c23efbdbd3b4f7afda67d954417a8c3792e0
Charles Pletcher <cpletcher2@gmail.com>
2018-09-08 11:29:52 -0400

    Pull in dev config from hul-frontend app

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,50 @@
-const path = require('path')
+const eslintFormatter = require('react-dev-utils/eslintFormatter');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './app/web/index.js',
+  devtool: 'cheap-module-source-map',
+  entry: [
+    require.resolve('react-dev-utils/webpackHotDevClient'),
+    './app/web/index.js'
+  ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        loader: require.resolve('babel-loader'),
+        options: {
+          cacheDirectory: true,
+        }
       }
-    ]
+    ],
+    strictExportPresence: true,
+  },
+  node: {
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty',
   },
   output: {
+    chunkFilename: '[name].chunk.js',
+    filename: 'webpack_bundle.js',
     path: path.resolve(__dirname, 'app/assets/javascripts/spotlight'),
+    pathinfo: true,
     publicPath: "/",
-    filename: 'webpack_bundle.js'
   },
+  performance: {
+    hints: false,
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+  ],
   resolve: {
     extensions: ['*', '.js', '.jsx']
   },
   devServer: {
     contentBase: path.resolve('./app/assets/javascripts/spotlight')
   }
-}
+};
