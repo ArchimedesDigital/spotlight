98d5866d934f236214e74cb547175e2fba5b5325
Charles Pletcher <cpletcher2@gmail.com>
2018-09-11 19:41:56 -0400

    Start working on other warnings

--- webpack.config.js
+++ webpack.config.js
@@ -28,8 +28,7 @@ module.exports = {
 					{
 						options: {
 							formatter: eslintFormatter,
-							eslintPath: require.resolve('eslint'),
-
+							eslintPath: require.resolve('eslint')
 						},
 						loader: require.resolve('eslint-loader'),
 					},
