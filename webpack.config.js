bb299f34639af2ec17b895585d971ee0f6a36b24
Charles Pletcher <cpletcher2@gmail.com>
2018-09-15 16:04:03 -0400

    Update config and frontend

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,73 @@
 const autoprefixer = require('autoprefixer');
 const eslintFormatter = require('react-dev-utils/eslintFormatter');
+const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
 
+const NODE_ENV = process.env.NODE_ENV;
+if (!NODE_ENV) {
+  throw new Error(
+    'The NODE_ENV environment variable is required but was not specified.'
+  );
+}
+
 const paths = {
-	appSrc: path.resolve(__dirname, 'app/web')
+	appSrc: path.resolve(__dirname, 'app/web'),
+	dotenv: path.resolve(__dirname)
+};
+
+// https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
+var dotenvFiles = [
+  `${paths.dotenv}.${NODE_ENV}.local`,
+  `${paths.dotenv}.${NODE_ENV}`,
+  // Don't include `.env.local` for `test` environment
+  // since normally you expect tests to produce the same
+  // results for everyone
+  NODE_ENV !== 'test' && `${paths.dotenv}.local`,
+  paths.dotenv,
+].filter(Boolean);
+
+// Load environment variables from .env* files. Suppress warnings using silent
+// if this file is missing. dotenv will never modify any environment variables
+// that have already been set.
+// https://github.com/motdotla/dotenv
+dotenvFiles.forEach(dotenvFile => {
+  if (fs.existsSync(dotenvFile)) {
+    require('dotenv').config({
+      path: dotenvFile,
+    });
+  }
+});
+
+// Grab NODE_ENV and REACT_APP_* environment variables and prepare them to be
+// injected into the application via DefinePlugin in Webpack configuration.
+const REACT_APP = /^REACT_APP_/i;
+
+function getClientEnvironment() {
+  const raw = Object
+		.keys(process.env)
+    .filter(key => REACT_APP.test(key))
+    .reduce(
+      (env, key) => {
+        env[key] = process.env[key];
+        return env;
+      },
+      {
+        // Useful for determining whether we’re running in production mode.
+        // Most importantly, it switches React into the correct mode.
+        NODE_ENV: process.env.NODE_ENV || 'development',
+      }
+    );
+
+	// Stringify all values so we can feed into Webpack DefinePlugin
+  const stringified = {
+		'process.env': Object.keys(raw).reduce((env, key) => {
+			env[key] = JSON.stringify(raw[key]);
+			return env;
+    }, {}),
+  };
+
+  return stringified;
 }
 
 module.exports = {
@@ -143,9 +206,7 @@ module.exports = {
 		hints: false,
 	},
 	plugins: [
-		new webpack.EnvironmentPlugin({
-			NODE_ENV: 'development',
-		}),
+		new webpack.EnvironmentPlugin(getClientEnvironment()),
 		new webpack.NamedModulesPlugin()
 	],
 	resolve: {
